714ee162bf1290cbec6b16f9f3160ad3e7d4ebcf
Katriel Chiu <katriel_chiu@msn.com>
2024-05-01 00:12:31 -0700

    implemented routes for tag model and tested each route successfully in Insomnia

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -25,6 +25,12 @@ router.get('/:id', async (req, res) => {
     const productData = await Product.findByPk(req.params.id, {
       include: [{ model: Category }, { model: Tag, through: ProductTag }]
     });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!'});
+      return;
+    }
+
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
